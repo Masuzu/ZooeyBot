a890a76cd74f07d008e6eb2e910fb7e35663d5ad
Masuzu <anveena.menethil@gmail.com>
2017-09-28 23:33:55 +0200

    Update test_dynamic_status_effect_retrieval.lua

--- Scripts/test_dynamic_status_effect_retrieval.lua
+++ Scripts/test_dynamic_status_effect_retrieval.lua
@@ -1,7 +1,6 @@
 Summon(6)
 DisableChargeAttack();
 characters["Zooey"]:WithWaitTime(4100):UseSkill(2)
-Refresh()
 if characters["<main_character>"]:HasStatusEffect("1028") then
   characters["<main_character>"]:UseSkill(1)
 end
