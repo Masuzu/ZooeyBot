6db0f5ec0b763277f2b096342bf1a9e4ba8a74d1
Masuzu <anveena.menethil@gmail.com>
2017-10-03 14:45:58 +0200

    Update and rename light_gw_ex.lua to light_gw_ex_celeste.lua

--- Scripts/light_gw_ex_celeste.lua
+++ Scripts/light_gw_ex_celeste.lua
@@ -3,21 +3,21 @@ Support summon: Bahamut or max limit break Celeste, force summon reset if not av
 DisableChargeAttack()
 if turn == 1 then
   character_1:UseSkill(4)
-  character_1:UseSkill(1):WithWaitTime(3600)
+  character_1:WithWaitTime(3600):UseSkill(1)
   character_1:UseSkill(3)
-  character_2:UseSkill(1):WithWaitTime(1000)
+  character_2:WithWaitTime(1000):UseSkill(1)
   --[[Use Zooey's Conjunction--]] 
-  character_3:UseSkill(2):WithWaitTime(4100)
+  character_3:WithWaitTime(4100):UseSkill(2)
   character_3:UseSkill(1)
   character_3:UseSkill(3)
-  character_2:UseSkill(3):WithWaitTime(1000)
+  character_2:WithWaitTime(1000):UseSkill(3)
   character_4:UseSkill(2)
   character_4:UseSkill(1)
 end
 if turn == 2 then
   --[[Use Clarity, which is bound to the second skill in my case--]] 
   if (enemy_1.hp / enemy_1.max_hp < 0.5) then
-    character_1:UseSkill(2):WithWaitTime(1000)
+    character_1:WithWaitTime(1000):UseSkill(2)
   end
   --[[Use Narmaya's ODA buff--]] 
   character_4:UseSkill(3)
