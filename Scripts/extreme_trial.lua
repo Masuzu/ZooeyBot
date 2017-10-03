53e62e8e56fe3f4d879903abd852b9fcab1c5108
Masuzu <anveena.menethil@gmail.com>
2017-10-03 14:46:20 +0200

    Update extreme_trial.lua

--- Scripts/extreme_trial.lua
+++ Scripts/extreme_trial.lua
@@ -1,5 +1,5 @@
 if battle == 3 then
-  characters["Zooey"]:UseSkill(2)
+  characters["Zooey"]:WithWaitTime(4100):UseSkill(2)
   characters["Zooey"]:UseSkill(1)
   characters["Orchid"]:UseSkill(3)
 end
