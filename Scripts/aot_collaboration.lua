892ca53b237214186a12d5d0bfe061d28b5567e2
Masuzu <anveena.menethil@gmail.com>
2017-12-08 23:16:30 +0100

    Create aot_collaboration.lua

---
+++ Scripts/aot_collaboration.lua
@@ -0,0 +1,11 @@
+EnableChargeAttack()
+if battle == 1 then
+  characters["Narmaya"]:UseSkill(1)
+end
+if battle == 3 then
+  characters["Zooey"]:WithWaitTime(3700):UseSkill(2)
+  character_1:UseSkill(2)
+  character_1:UseSkill(3)
+  character_1:UseSkill(4)
+  character_4:UseSkill(1)
+end
